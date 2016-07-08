9dca0be2af071f7c191cbd7795227ca6271c9855
CaseyManning <kcgrind@gmail.com>
2016-07-08 10:43:06 -0700

    fixed dying bug and goal bug

--- PuzzleJam/EnemyBackAndForth.swift
+++ PuzzleJam/EnemyBackAndForth.swift
@@ -12,15 +12,16 @@ import SpriteKit
 class EnemyBackAndForth: Enemy {
     
     var back = false
-    var mapX = 0
+    var mapX = 5
     var mapY = 1
-    var mapZ = 0
+    var mapZ = 5
     
-    init(x: Int, y: Int) {
+    init(location: Vector3) {
         super.init()
-        sprite.position = CGPointMake(CGFloat(x*GameScene.datileWidth), CGFloat(y*GameScene.datileHeight))
-        mapX = x
-        mapY = y
+        sprite.position = CGPointMake(CGFloat(location.x*GameScene.datileWidth), CGFloat(location.z*GameScene.datileHeight))
+        mapX = location.x
+        mapY = location.y
+        mapZ = location.z
         sprite.position.y += CGFloat(GameScene.datileHeight) + 15
         sprite.position.x += CGFloat(GameScene.datileWidth/2)
     }
@@ -43,6 +44,9 @@ class EnemyBackAndForth: Enemy {
                 return
             }
         } else {
+            print(mapX)
+            print(mapY)
+            print(mapZ)
             if map[mapY][mapZ][mapX + 1] == 0 {
                 
                 mapX += 1
